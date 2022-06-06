b138f4a09362d2492358b6bbe1c7f55de3593ce3
Fran Dios <fran.dios@shopify.com>
2022-06-06 12:06:26 +0900

    Update to latest Vercel Build Output API v3

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,11 @@ import hydrogen from '@shopify/hydrogen/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [hydrogen()],
   optimizeDeps: {include: ['@headlessui/react']},
-  plugins: [hydrogen(), readableStreamWorkaround()],
+  test: {
+    globals: true,
+    testTimeout: 10000,
+    hookTimeout: 10000,
+  },
 });
-
-function readableStreamWorkaround() {
-  let config;
-  return {
-    name: 'readable-stream-workaround',
-    configResolved(_config) {
-      config = _config;
-    },
-    transform(code, id) {
-      if (config.command === 'build' && id.includes('streaming.server.js')) {
-        return code.replace('let cachedStreamingSupport', '$& = false');
-      }
-    },
-  };
-}
